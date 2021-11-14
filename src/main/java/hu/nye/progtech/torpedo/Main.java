b2d090e59a2520b29b0be31737da2b90261b9b04
Zsolt Vaszily <zvaszily@gmail.com>
2021-11-14 19:36:27 +0100

    Added TableCreator random ships

--- src/main/java/hu/nye/progtech/torpedo/Main.java
+++ src/main/java/hu/nye/progtech/torpedo/Main.java
@@ -1,17 +1,21 @@
 package hu.nye.progtech.torpedo;
 
 import hu.nye.progtech.torpedo.model.BaseTable;
+import hu.nye.progtech.torpedo.service.TableCreator;
 
 public class Main {
     public static void main(String[] args) {
 
         char[][] map = {
-                {' ', 'X'},
+                {' ', '¤'},
                 {'X', ' '}
         };
 
         BaseTable baseTable = new BaseTable(2,2,map);
 
         System.out.println(baseTable);
+
+        TableCreator tableCreator = new TableCreator(10,10);
+        System.out.println(tableCreator.createTable());
     }
 }
--- src/main/java/hu/nye/progtech/torpedo/service/TableCreator.java
+++ src/main/java/hu/nye/progtech/torpedo/service/TableCreator.java
@@ -21,6 +21,46 @@ public class TableCreator {
     private char[][] getMap(){
         char[][] map = new char[numberOfRows][numberOfColumns];
 
+        for(int shipSize=5;shipSize>0;shipSize--){
+            createShip(map,shipSize);
+
+
+        }
+
        return map;
+    }
+
+    private void createShip(char[][] map,int shipSize){
+        int max;
+        int shipPlace;
+        if(shipHorizont()){
+            max = numberOfColumns-shipSize;
+            shipPlace = shipPlace(max);
+            System.out.println(shipPlace);
+            for(int j=0;j<shipSize;j++){
+                map[shipPlace][j]='¤';
+            }
+        }else{
+            max = numberOfRows-shipSize;
+            shipPlace = shipPlace(max);
+            System.out.println(shipPlace);
+            for(int j=0;j<shipSize;j++){
+                map[j][shipPlace]='X';
+            }
+        }
+    }
+
+    private boolean shipHorizont(){
+        return Math.random() > 0.5;
+    }
+
+    private int shipPlace(int max){
+        return (int)(Math.random()*max);
+    }
+
+    private boolean validShipPlace(char[][] map,int shipSize,int shipPlace,boolean shshipHorizont){
+        boolean shipPlaceValid;
+
+        return false;
     }
 }
